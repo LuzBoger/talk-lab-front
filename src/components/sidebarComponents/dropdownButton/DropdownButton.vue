09547ca15c22a6fda423afcf59e624acb0f8e475
LuzBoger <arthurbrouard2003@gmail.com>
2025-06-21 11:32:30 +0200

    creation du drowDownButton pour le menu

---
+++ src/components/sidebarComponents/dropdownButton/DropdownButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    @click="action"
+    class="flex gap-2 bg-none border-none text-text-primary p-2.5 text-left w-full rounded-lg text-sm cursor-pointer transition-colors duration-200 hover:bg-dropdown-hover"
+  >
+    <slot name="icon"></slot>
+    <span> {{ label }} </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'DropdownButton',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    action: {
+      type: Function,
+      required: true,
+    },
+  },
+}
+</script>
