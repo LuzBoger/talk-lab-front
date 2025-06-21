d6944e76daa2cf8ec1aaf3497467c99c7b192d09
LuzBoger <arthurbrouard2003@gmail.com>
2025-06-21 11:34:03 +0200

    simplification du default layout

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,59 +1,32 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import type { User } from '../types/User';
-import authService from '../api/authService';
-import Sidebar from '../components/Sidebar.vue';
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import type { User } from '../types/User'
+import authService from '../api/authService'
+import Sidebar from '../components/Sidebar.vue'
 
-const user = ref<User | null>(null);
-const router = useRouter();
-const sidebarCollapsed = ref(false);
+const user = ref<User | null>(null)
+const router = useRouter()
 
 const logout = () => {
-  authService.logout();
-  user.value = null;
-  router.push('/login');
-};
-
-
-const handleSidebarToggle = (collapsed: boolean) => {
-  sidebarCollapsed.value = collapsed;
-};
-
-
-
+  authService.logout()
+  user.value = null
+  router.push('/login')
+}
 
 onMounted(() => {
-  const currentUser = authService.getCurrentUser();
+  const currentUser = authService.getCurrentUser()
   if (currentUser) {
-    user.value = currentUser;
+    user.value = currentUser
   }
-});
+})
 </script>
 
 <template>
-  <div class="flex min-h-screen bg-dark">
-    <Sidebar 
-      @sidebar-toggle="handleSidebarToggle" 
-      @logout="logout"
-      :isLoggedIn="!!user"
-    />
-    
-    <div class="flex-1 bg-[#1D1C31]" :class="{ 'ml-20': sidebarCollapsed, 'ml-305': !sidebarCollapsed }">
-        <router-view />
+  <div class="flex">
+    <Sidebar @logout="logout" :isLoggedIn="!!user" />
+    <div class="flex flex-1 bg-[#1D1C31]">
+      <router-view />
     </div>
   </div>
 </template>
-
-<style scoped>
-
-.ml-305 {
-  margin-left: 305px;
-}
-
-.ml-20 {
-  margin-left: 80px;
-}
-
-
-</style>
