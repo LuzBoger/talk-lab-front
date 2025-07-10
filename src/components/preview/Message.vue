0ff2ea0a69a7eea92cdf0c8d43e881d38efe8b74
LuzBoger <arthurbrouard2003@gmail.com>
2025-07-10 10:04:17 +0200

    modification pour gérer l'affichage de tous les types de message

--- src/components/preview/Message.vue
+++ src/components/preview/Message.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import Reaction from '../Reaction.vue'
 import clsx from 'clsx'
+import type { Messages } from '../../types/Messages'
+import Vocal from './Vocal.vue'
+
 const props = defineProps({
-  message: Object,
+  message: {
+    type: Object as () => Messages,
+    required: true,
+  },
   color: String,
   radiusClass: String,
   index: Number,
@@ -12,40 +18,84 @@ const props = defineProps({
     required: true,
   },
 })
+
 const showReaction = ref<boolean>(false)
+const showPlusButton = ref<boolean>(false)
+const imageUrl = ref<string | null>(null)
+
 const toggleReaction = () => {
-  console.log(showReaction.value)
   showReaction.value = !showReaction.value
 }
-console.log(props.message)
+
+const handleReactionSelected = (emoji: string) => {
+  props.addReaction(props.index, emoji)
+  showReaction.value = false
+  showPlusButton.value = false
+}
+
+onMounted(() => {
+  if (props.message?.image) {
+    imageUrl.value = URL.createObjectURL(props.message.image)
+  }
+})
+
+onBeforeUnmount(() => {
+  if (imageUrl.value) {
+    URL.revokeObjectURL(imageUrl.value)
+  }
+})
+console.log('props.radiusClass:', props.radiusClass)
 </script>
 <template>
   <div
     :class="
       clsx(
-        'max-w-[256px] px-[13.5px] py-[9px] w-fit my-[1px] leading-4.5 text-base rounded-[18px] flex items-center cursor-pointer relative',
+        'max-w-[256px] px-[13.5px] py-[9px] w-fit my-[1px] leading-4.5 text-base flex items-center cursor-pointer relative group',
         props.radiusClass,
-        props.color,
-        props.message.reaction !== '' && 'mb-5'
+        props.message?.image ? '' : props.color,
+        props.message.reaction !== '' && 'mb-5',
       )
     "
-    @click="toggleReaction"
+    @mouseover="showPlusButton = true"
+    @mouseleave="showPlusButton = false"
   >
     <span>{{ props.message.message }}</span>
+    <img
+      v-if="imageUrl"
+      :src="imageUrl"
+      alt="Image from Blob"
+      class="rounded-[18px]"
+    />
     <span
       v-if="props.message.reaction !== ''"
       :class="
         clsx(
-          'absolute top-7 left-0 rounded-full px-1.5 py-0.5 border-2 border-white',
+          'absolute left-0 rounded-full px-1.5 py-0.5 border-2 border-white',
           props.color,
+          props.message?.image ? 'bottom-[-8px]' : 'bottom-[-22px]',
         )
       "
-      >{{ props.message.reaction }}</span
     >
-
+      {{ props.message.reaction }}
+    </span>
+    <Vocal
+      v-if="props.message.audio"
+      :audio="props.message.audio"
+      :author="props.message.author"
+    />
+    <button
+      v-if="showPlusButton"
+      @click.stop="toggleReaction"
+      :class="[
+        'absolute left-0 rounded-full px-3 py-0.5 border-2 bg-white border-white text-green-700 cursor-pointer bottom-[-22px] z-50',
+      ]"
+    >
+      +
+    </button>
     <Reaction
       v-if="showReaction"
-      @selected="(emoji) => props.addReaction(props.index, emoji)"
+      :author="props.message.author"
+      @selected="(emoji) => handleReactionSelected(emoji)"
     />
   </div>
 </template>
