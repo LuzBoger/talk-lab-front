aa9d8b783fa9da4d92cd1e4d3a671db5026fefe9
LuzBoger <arthurbrouard2003@gmail.com>
2025-07-12 16:30:51 +0200

    ajout d'un rooter link lors du click sur le logo

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -58,7 +58,9 @@ onMounted(() => {
     class="w-72 h-screen bg-sidebar-bg p-5 flex flex-col justify-between text-text-primary"
   >
     <div class="logo flex justify-center mb-10">
-      <LogoIcon />
+      <router-link to="/" class="cursor-pointer">
+        <LogoIcon />
+      </router-link>
     </div>
 
     <nav class="flex flex-col flex-1">
