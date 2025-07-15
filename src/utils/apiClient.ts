08b9f343482acac064f4a546910a86fa97e3bf99
LuzBoger <arthurbrouard2003@gmail.com>
2025-07-15 22:49:47 +0200

    delete caca

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import authService from '../api/authService'
 import { useAuthStore } from '../stores/useAuthStore'
-console.log('caca')
 console.log(import.meta.env.VITE_API_URL || '__VITE_API_URL__')
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL || '__VITE_API_URL__',
