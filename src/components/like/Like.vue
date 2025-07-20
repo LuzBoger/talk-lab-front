ce2f06e6e3ee68b242c135467bf1d3d9b9a478a4
LuzBoger <arthurbrouard2003@gmail.com>
2025-07-20 12:26:54 +0200

    modification affichage like

--- src/components/like/Like.vue
+++ src/components/like/Like.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { ref, onMounted, computed } from 'vue'
-import { useAuthStore } from '../../stores/useAuthStore';
-import { getLikesCount, getLikers, addLike, removeLike } from '../../api/conversation';
-import HandThumbUp from '../icon/HandThumbUp.vue';
-import type { Likers } from '../../types/Likers';
-
+import { useAuthStore } from '../../stores/useAuthStore'
+import {
+  getLikesCount,
+  getLikers,
+  addLike,
+  removeLike,
+} from '../../api/conversation'
+import HandThumbUp from '../icon/HandThumbUp.vue'
+import type { Likers } from '../../types/Likers'
+import LikeIcon from '../icon/LikeIcon.vue'
 
 const props = defineProps<{
-    conversationId: number
+  conversationId: number
 }>()
 
 const authStore = useAuthStore()
@@ -18,64 +23,73 @@ const error = ref<string>('')
 const isModalOpen = ref<boolean>(false)
 
 const currentUserId = computed(() => {
-    return authStore.user?.id;
+  return authStore.user?.id
 })
 
-
 const fetchLikes = async () => {
-    likesCount.value = await getLikesCount(props.conversationId)
-    likers.value = await getLikers(props.conversationId)
-    isLiked.value = likers.value.some((user) => user.id === currentUserId.value)
+  likesCount.value = await getLikesCount(props.conversationId)
+  likers.value = await getLikers(props.conversationId)
+  isLiked.value = likers.value.some((user) => user.id === currentUserId.value)
 }
 
-
 const toggleLike = async () => {
-    if (!currentUserId.value) {
-        return
+  if (!currentUserId.value) {
+    return
+  }
+  try {
+    if (isLiked.value) {
+      await removeLike(props.conversationId)
+    } else {
+      await addLike(props.conversationId)
     }
-    try {
-        if (isLiked.value) {
-            await removeLike(props.conversationId)
-        } else {
-            await addLike(props.conversationId)
-        }
-        await fetchLikes()
-    } catch (err) {
-        error.value = 'Erreure lors de l\'ajout d\'un like ou dislike'
-
-    }
+    await fetchLikes()
+  } catch (err) {
+    error.value = "Erreure lors de l'ajout d'un like ou dislike"
+  }
 }
 
 const openModal = () => {
-    isModalOpen.value = true
+  isModalOpen.value = true
 }
 const closeModal = () => {
-    isModalOpen.value = false
+  isModalOpen.value = false
 }
 
-
-
 onMounted(async () => {
-    await fetchLikes()
+  await fetchLikes()
 })
 </script>
 <template>
-    <div class="flex items-center space-x-4">
-        <button @click="toggleLike"  class="flex items-center space-x-1 text-white" type="button" :disabled="!currentUserId">
-<HandThumbUp
-  :filled="isLiked"
-  :class="[isLiked ? 'text-green-500' : 'text-gray-400']"
-/>
-         <span v-if="currentUserId" @click.stop="openModal"
-                class="text-sm text-gray-300 hover:text-white cursor-pointer">
-                {{ likesCount }}
-            </span>
-            <span v-else class="text-sm text-gray-300 cursor-default">
-                {{ likesCount }}
-            </span>
-        </button>
-        <slot name="modal" :is-modal-open="isModalOpen" :conversation-id="props.conversationId" :close="closeModal"></slot>
-    </div>
+  <div class="flex items-center space-x-4">
+    <button
+      @click="toggleLike"
+      class="flex items-center space-x-1 text-white cursor-pointer hover:bg-gray-700/30 p-1 rounded-lg"
+      type="button"
+      :disabled="!currentUserId"
+    >
+      <LikeIcon
+        :width="24"
+        :height="24"
+        :fillColor="isLiked ? 'green' : 'gray'"
+      />
+      <span
+        v-if="currentUserId"
+        @click.stop="openModal"
+        class="text-sm text-gray-300 hover:text-white cursor-pointer"
+      >
+        {{ likesCount }}
+      </span>
+      <span v-else class="text-sm text-gray-300 cursor-default">
+        {{ likesCount }}
+      </span>
+    </button>
+    <slot
+      name="modal"
+      :is-modal-open="isModalOpen"
+      :conversation-id="props.conversationId"
+      :close="closeModal"
+    ></slot>
+  </div>
 
-    <p v-if="error" class="text-red-500 text-sm mt-2">{{ error }}</p>
+  <p v-if="error" class="text-red-500 text-sm mt-2">{{ error }}</p>
 </template>
