43a6e1e9e66600a989b38c3d79baf47ba1629f40
LuzBoger <arthurbrouard2003@gmail.com>
2025-07-20 18:33:56 +0200

    fix: build

--- src/components/like/LikeCount.vue
+++ src/components/like/LikeCount.vue
@@ -1,32 +1,42 @@
-<script setup lang="ts" >
-import  {ref, onMounted} from 'vue'
-import { getLikesCount } from '../../api/conversation';
-import LikeModal from './LikeModal.vue';
-import HandThumbUp from '../icon/HandThumbUp.vue';
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { getLikesCount } from '../../api/conversation'
+import LikeModal from './LikeModal.vue'
+import LikeIcon from '../icon/LikeIcon.vue'
 
 const props = defineProps<{
-    conversationId: number
+  conversationId: number
 }>()
 
 const likesCount = ref<number>(0)
 const isModalOpen = ref<boolean>(false)
 
-
 const fetchLikeCount = async () => {
-    likesCount.value = await getLikesCount(props.conversationId)
+  likesCount.value = await getLikesCount(props.conversationId)
 }
 
-
 onMounted(() => {
-    fetchLikeCount()
+  fetchLikeCount()
 })
 </script>
 
 <template>
-    <div class="flex items-center space-x-2 text-white cursor-pointer" @click="isModalOpen = true">
-        <HandThumbUp class="w-5 h-5 text-green-500" />
-        <span>{{ likesCount }}</span>
+  <div
+    class="flex items-center space-x-2 text-white cursor-pointer"
+    @click="isModalOpen = true"
+  >
+    <LikeIcon
+      :width="24"
+      :height="24"
+      :fillColor="'green'"
+      className="w-5 h-5"
+    />
+    <span>{{ likesCount }}</span>
 
-        <LikeModal :conversation-id="props.conversationId" :is-modal-open="isModalOpen" @close="isModalOpen = false" />
-    </div>
+    <LikeModal
+      :conversation-id="props.conversationId"
+      :is-modal-open="isModalOpen"
+      @close="isModalOpen = false"
+    />
+  </div>
 </template>
